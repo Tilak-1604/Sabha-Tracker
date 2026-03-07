465137928dc28b9db4c533e66d6be6105d39aaac
Tilak-1604 <vaghasiyatilak16@gmail.com>
2026-03-07 17:13:10 +0530

    a

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.26fv8pisjt"
+    "revision": "0.1bllqljuv88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
